5824bb341e5fc81340e3075b357c95be84eef756
Probit Jyoti Kalita <probitjkalita@disroot.org>
2023-12-01 01:43:25 +0530

    Completed part 2
    Was pretty simple. Could have merged both solutions into a single
    function but I am not going for optimization here.

--- AoC_2015/day_1/day_1.py
+++ AoC_2015/day_1/day_1.py
@@ -6,6 +6,18 @@ Why just start with 2023 when I have the entire catalogue of previous AoCs to tr
 """
 
 
+def when_basement(inp_text):
+    """Find which move sends Santa to the basement."""
+    floor = 0
+    entry_point = 0
+    for position, letter in enumerate(inp_text):
+        floor += 1 if letter == "(" else -1
+        if floor == -1:
+            entry_point = position + 1
+            break
+    return entry_point
+
+
 def which_floor(inp_text):
     """LSP keeps nagging me for a docstring, so here you go."""
     floor = 0
@@ -14,8 +26,9 @@ def which_floor(inp_text):
     return floor
 
 
-with open("input.txt") as inp_file:
+with open("input.txt", encoding="utf-8") as inp_file:
     text = inp_file.readline()
     text = text[:-1]
     FINAL_FLOOR = which_floor(text)
-    print(FINAL_FLOOR)
+    BASEMENT_ENTRY = when_basement(text)
+    print(FINAL_FLOOR, BASEMENT_ENTRY)
