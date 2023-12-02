781672227a5ef4e1bf1eba2c98fc61813505c761
ProbitJK (Lab) <522ph1008@nitrkl.ac.in>
2023-12-03 04:04:14 +0530

    Finally got the time to complete part two
    Looks to be too much of a hacky solution for me, maybe using regex would
    improve it

--- AoC_2023/day_1/day_1.py
+++ AoC_2023/day_1/day_1.py
@@ -1,26 +1,35 @@
 #!/usr/bin/env python3
 
 
+starter_table = ["e", "f", "n", "o", "t", "s"]
+conv_table = [
+    ["one", 1],
+    ["two", 2],
+    ["three", 3],
+    ["four", 4],
+    ["five", 5],
+    ["six", 6],
+    ["seven", 7],
+    ["eight", 8],
+    ["nine", 9],
+]
+
+
 def decode(gibberish):
-    leng = len(gibberish)
-    tens = []
-    ones = []
-    for i in range(leng):
-        if gibberish[i].isdigit():
-            tens.append(int(gibberish[i]))
-        if gibberish[leng - i - 1].isdigit():
-            ones.append(int(gibberish[leng - i - 1]))
-    num = 10 * tens[0] + ones[0]
+    nums = []
+    for i, character in enumerate(gibberish):
+        if character.isdigit():
+            nums.append(int(character))
+        elif character in starter_table:
+            for j in range(9):
+                if gibberish[i:].startswith(conv_table[j][0]):
+                    nums.append(conv_table[j][1])
+    num = 10 * nums[0] + nums[-1]
     return num
 
 
-example = "treb7uchet"
-
-calibration = decode(example)
-print(calibration)
-
 sum = 0
-with open('input.txt', encoding='utf-8') as in_file:
+with open("input.txt", encoding="utf-8") as in_file:
     lines = in_file.readlines()
     for text in lines:
         sum += decode(text)
