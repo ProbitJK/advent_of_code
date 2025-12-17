da21e9fa4f92de6141273d0c69007296409faffc
ProbitJK (Lab) <522ph1008@nitrkl.ac.in>
2025-12-17 19:03:18 +0530

    completed 2025 day 2 both puzzles
    That took an embarrasingly long time to solve the problem

--- AoC_2025/day_2/day_2.py
+++ AoC_2025/day_2/day_2.py
@@ -6,20 +6,40 @@ for block in blocks:
     ends = block.split("-")
     lower = int(ends[0])
     upper = int(ends[1])
-    for i in range(lower, upper+1):
+    for i in range(lower, upper + 1):
         nums_to_check.append(i)
 invalid_sum: int = 0
 for num in nums_to_check:
     numstr = str(num)
     leng = len(numstr)
-    if leng%2==0:
-        p1 = numstr[: int(leng/2)]
-        p2 = numstr[int(leng/2) :]
+    if leng % 2 == 0:
+        p1 = numstr[: int(leng / 2)]
+        p2 = numstr[int(leng / 2) :]
         if p1 == p2:
             invalid_sum += num
 print(invalid_sum)
-# new_invalid = 0
-# for num in nums_to_check[10000:10001]:
-#     numstr = str(num)
-#     maxidx = int(len(numstr)/2)
-#     splits = []
+new_invalid_sum: int = 0
+for num in nums_to_check:
+    NUM_S = str(num)
+    MAX_BLOCKS = len(NUM_S)
+    num_blocks = 2
+    while num_blocks <= MAX_BLOCKS:
+        if MAX_BLOCKS % num_blocks == 0:
+            parts: list[str] = []
+            block_size = int(MAX_BLOCKS / num_blocks)
+            for i in range(0, MAX_BLOCKS, block_size):
+                parts.append(NUM_S[i : i + block_size])
+            condi = False
+            for i in range(1, num_blocks):
+                if parts[i] == parts[i-1]:
+                    condi = True
+                else:
+                    condi = False
+                    break
+            if condi==True:
+                new_invalid_sum += num
+                break
+            num_blocks += 1
+        else:
+            num_blocks += 1
+print(new_invalid_sum)
