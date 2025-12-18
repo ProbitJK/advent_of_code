c1d504ce9b880197d601df1c5bae1c7eccf403ce
ProbitJK (Lab) <522ph1008@nitrkl.ac.in>
2025-12-18 20:56:26 +0530

    completed day 3 part 2; implementation of logic needed working on

--- AoC_2025/day_3/day_3.py
+++ AoC_2025/day_3/day_3.py
@@ -1,4 +1,4 @@
-with open("test.txt", mode="r", encoding="utf-8") as f:
+with open("input.txt", mode="r", encoding="utf-8") as f:
     all_banks = [line.strip() for line in f]
 total_joltage = 0
 for bank in all_banks:
@@ -12,32 +12,14 @@ for bank in all_banks:
     bank_joltage = int(max_jolt) * 10 + int(nex_max_jolt)
     total_joltage += bank_joltage
 print("total output joltage ", total_joltage)
-# there is some flaw in the logic that is making it not work
-# larger_total_joltage = 0
-# for bank in all_banks:
-#     bank_length = len(bank)
-#     bank_jolts = []
-#     for i in range(bank_length):
-#         bank_jolts.append(bank[i : i + 1])
-#     bank_big_jolts = []
-#     bank_big_jolts.append(max(bank_jolts[:-11]))
-#     startind = bank_jolts.index(bank_big_jolts[0])
-#     print(bank_jolts)
-#     print(bank_big_jolts, startind)
-#     while True:
-#         nex_jolt = max(bank_jolts[startind + 1 :])
-#         nex_jolt_idx = bank_jolts[startind + 1 :].index(nex_jolt)
-#         print(nex_jolt, nex_jolt_idx)
-#         if len(bank_big_jolts) + len(bank_jolts[startind + nex_jolt_idx :]) >= 12:
-#             bank_big_jolts.append(nex_jolt)
-#             startind += nex_jolt_idx
-#             print(bank_big_jolts, startind)
-#         else:
-#             nex_jolt = max(bank_jolts[startind + 1 : nex_jolt_idx])
-#             nex_jolt_idx = bank_jolts[startind + 1 : nex_jolt_idx -1].index(nex_jolt)
-#             bank_big_jolts.append(nex_jolt)
-#             startind += nex_jolt_idx
-#             print(bank_big_jolts, startind)
-#         if len(bank_big_jolts)==12:
-#             print(bank_big_jolts)
-#             break
+new_total_joltage = 0
+for bank_line in all_banks:
+    bank = list(map(int, bank_line))
+    bank_joltage = 0
+    for i in range(11):
+        jolt = max(bank[: i - 11])
+        bank = bank[bank.index(jolt) + 1 :]
+        bank_joltage = (bank_joltage * 10) + jolt
+    bank_joltage = (bank_joltage * 10) + max(bank)
+    new_total_joltage += bank_joltage
+print("New total output joltage ", new_total_joltage)
